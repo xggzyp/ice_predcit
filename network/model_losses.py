eb547e90f8fd7c3bbbb8f914a8aea1bcab910e07
Justin <614613193@qq.com>
2020-11-19 18:07:01 +0800

    zengjia model.loss

---
+++ network/model_losses.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Dependency imports
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
+
+# Useful for REINFORCE baseline.
+from losses import losses
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def create_dis_loss(fake_predictions, real_predictions, targets_present):
+    """Compute Discriminator loss across real/fake."""
+
+    missing = tf.cast(targets_present, tf.int32)
+    missing = 1 - missing
+    missing = tf.cast(missing, tf.bool)
+
+    real_labels = tf.ones([FLAGS.batch_size, FLAGS.sequence_length])
+    dis_loss_real = tf.losses.sigmoid_cross_entropy(
+        real_labels, real_predictions, weights=missing)
+    dis_loss_fake = tf.losses.sigmoid_cross_entropy(
+        targets_present, fake_predictions, weights=missing)
+
+    dis_loss = (dis_loss_fake + dis_loss_real) / 2.
+    return dis_loss, dis_loss_fake, dis_loss_real
+
+
+def create_critic_loss(cumulative_rewards, estimated_values, present):
+    """Compute Critic loss in estimating the value function.  This should be an
+    estimate only for the missing elements."""
+    missing = tf.cast(present, tf.int32)
+    missing = 1 - missing
+    missing = tf.cast(missing, tf.bool)
+
+    loss = tf.losses.mean_squared_error(
+        labels=cumulative_rewards, predictions=estimated_values, weights=missing)
+    return loss
